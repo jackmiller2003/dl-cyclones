6313bfbe5a927e4df531abd7c1a7d1dbec218838
Jack Miller <millerjack2003@gmail.com>
2022-02-26 11:51:03 +1100

    Added some processing and graphing

--- tracks/tracks_module.py
+++ tracks/tracks_module.py
@@ -17,6 +17,8 @@ class Track:
             'coordinates':[],
             'wind_speeds':[],
             'pressures':[],
+            'basin':[],
+            'subbasin':[],
             'storm_id':0,
             'data_len':0,
             'season':0
@@ -33,9 +35,11 @@ class Track:
         for t_row in raw_track:
             self.data['iso_times'].append(t_row['ISO_TIME'])
             self.data['categories'].append(t_row['USA_SSHS'])
-            self.data['coordinates'].append((t_row['LAT'], t_row['LON']))
+            self.data['coordinates'].append((t_row['LON'], t_row['LAT']))
             self.data['wind_speeds'].append(t_row['WMO_WIND'])
             self.data['pressures'].append(t_row['WMO_PRES'])
+            self.data['basin'].append(t_row['BASIN'])
+            self.data['subbasin'].append(t_row['SUBBASIN'])
     
     def save(self, json_file):
         add_to_json(self.sid, self.data, json_file)
@@ -54,7 +58,10 @@ def save_all_storms(file_name, save_to_file='tracks.json', year_init=1979):
             if i < 2:
                 continue
 
-            if (int(row['SEASON']) > year_init):
+            if row['TRACK_TYPE'] != 'main':
+                continue
+
+            if (int(row['SEASON']) >= year_init):
                 if current_raw_track == []:
                     current_raw_track = [row]
                     current_storm = row['SID']
@@ -68,9 +75,23 @@ def save_all_storms(file_name, save_to_file='tracks.json', year_init=1979):
                     current_storm = row['SID']
                 else:
                     current_raw_track.append(row)
-
             else:
                 continue
 
+def convert_lat_lon(tracks_file='tracks.json', proc_tracks_file='proc_tracks.json'):
+    with open('tracks.json', 'r') as tracks_json:
+        tracks_dict = json.load(tracks_json)
+    
+    for sid, data in tqdm(tracks_dict.items()):
+        new_dict = data
+        new_coords = []
+        for coordinate in data['coordinates']:
+            new_longitude = (float(coordinate[0]) % 360 + 540) % 360 - 180
+            new_coords.append((new_longitude, float(coordinate[1])))
+        new_dict['coordinates'] = new_coords
+        add_to_json(sid, new_dict, proc_tracks_file)
+
+
 if __name__ == '__main__':
-    save_all_storms('ibtracs.ALL.list.v04r00.csv', save_to_file="tracks.json", year_init=1979)
+    # save_all_storms('ibtracs.ALL.list.v04r00.csv', save_to_file="tracks.json", year_init=1979)
+    convert_lat_lon()
