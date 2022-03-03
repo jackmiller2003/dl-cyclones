65b5d1d7cd412e17ec290eb3515b79c22bb29d53
Jack Miller <millerjack2003@gmail.com>
2022-03-03 16:20:23 +1100

    Finished rough test_for_cpu

--- reanalysis/test_for_cpu.py
+++ reanalysis/test_for_cpu.py
@@ -15,13 +15,22 @@ def error_tolerant(f, *args, **kwargs):
     except Exception:
         traceback.print_exc()
 
-def get_some_cyclones():
+def get_some_cyclones(lower, upper):
     cyclone_thunks = []
 
-    for ssid in list(tracks.keys())[1000:1003]:
+    for ssid in list(tracks.keys())[lower:upper]:
         track = tracks[ssid]
         cyclone_thunks.append(dask.delayed(
-            error_tolerant(lib.track_to_ndarray, track['iso_times'], [(lat, long) for [long, lat] in track['coordinates']], degree_window=10)
+            error_tolerant(lib.track_to_ndarray, track['iso_times'], [(lat, long) for [long, lat] in track['coordinates']], levels=[200,450,650,750,850], degree_window=10)
         ))
 
     cyclones = dask.compute(*cyclone_thunks)
+
+    return cyclones, list(tracks.keys())[lower:upper]
+
+if __name__ == '__main__':
+    y, ssids = get_some_cyclones(1000,1002)
+
+    for i, cyclone in enumerate(y):
+        with open(f'/g/data/x77/jm0124/{ssids[i]}', 'wb') as f:
+            np.save(f, cyclone)
