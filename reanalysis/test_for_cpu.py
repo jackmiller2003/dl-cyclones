f20dc982181d6b3d2e3b980c98c38f374db14adb
Oliver Balfour <oliver.leo.balfour@gmail.com>
2022-03-05 14:48:22 +1100

    save each cyclone one by one

--- reanalysis/test_for_cpu.py
+++ reanalysis/test_for_cpu.py
@@ -16,28 +16,45 @@ def error_tolerant(f, *args, **kwargs):
     except Exception:
         traceback.print_exc()
 
-def get_some_cyclones(lower, upper):
+def get_user_path() -> str:
+    # I'm not sure if there's a general way to know what the username is so we'll hardcode Jack's for now
+    return f'/g/data/x77/jm0124'
+
+def job(track, ssid) -> None:
+    "Saves a binary ndarray for a cyclone given the track JSON and unique ID from the IBTrACS database"
+    try:
+        print(f"Getting ndarray for cyclone {ssid}")
+        s_time = time.perf_counter()
+        cyclone = lib.track_to_ndarray(
+            track['iso_times'],
+            [(lat, long) for [long, lat] in track['coordinates']],
+            levels=[200,450,650,750,850],
+            degree_window=10
+        )
+        print(f"Saving ndarray for cyclone {ssid}")
+        with open(f'{get_user_path()}/cyclone_binaries/{ssid}', 'wb') as f:
+            np.save(f, cyclone)
+        e_time = time.perf_counter()
+        print(f"Finished processing cyclone {ssid} in {e_time - s_time:.2f} seconds")
+    except Exception as e:
+        raise Exception(f"Failed to process cyclone {ssid}")
+
+def get_some_cyclones(lower, upper) -> None:
     cyclone_thunks = []
 
     for ssid in list(tracks.keys())[lower:upper]:
         track = tracks[ssid]
         cyclone_thunks.append(dask.delayed(
-            error_tolerant(lib.track_to_ndarray, track['iso_times'], [(lat, long) for [long, lat] in track['coordinates']], levels=[200,450,650,750,850], degree_window=10)
+            error_tolerant(job, track, ssid)
         ))
 
     print("Running dask compute")
-    cyclones = dask.compute(*cyclone_thunks)
+    dask.compute(*cyclone_thunks)
     print("Finished compute")
-    
-    return cyclones, list(tracks.keys())[lower:upper]
 
 if __name__ == '__main__':
     o_time = time.perf_counter()
-    y, ssids = get_some_cyclones(1010,1014)
+    get_some_cyclones(1010,1014)
     n_time = time.perf_counter()
-
-    for i, cyclone in enumerate(y):
-        with open(f'/g/data/x77/jm0124/cyclone_binaries/{ssids[i]}', 'wb') as f:
-            np.save(f, cyclone)
-
+    print(f"Finished processing ALL cyclones in {n_time-o_time:.2f} seconds")
     quit()
