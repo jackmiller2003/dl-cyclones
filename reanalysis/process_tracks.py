d34b16354a6ab18da7842166c87d32ce80777a94
Oliver Balfour <oliver.leo.balfour@gmail.com>
2022-03-28 16:14:59 +1100

    untested rewrite

--- reanalysis/process_tracks.py
+++ reanalysis/process_tracks.py
@@ -3,7 +3,7 @@ import dask.bag as db
 from dask.distributed import Client
 import traceback
 import json
-import track_to_ndarray as lib
+import track_lib as lib
 import numpy as np
 import os
 import xarray
@@ -20,54 +20,39 @@ def get_user_path() -> str:
     user = sys.argv[1]
     return f'/g/data/x77/{user}'
 
-def job(ssid) -> None:
+def create_netcdf_file_for_track(ssid) -> None:
     "Saves a binary ndarray for a cyclone given the track JSON and unique ID from the IBTrACS database"
 
     track = tracks[ssid]
-    N = None # subset track length
     print(f"Getting ndarray for cyclone {ssid}, length {len(track['iso_times'])}")
 
     n_workers = len(client.scheduler_info()["workers"])
     #p = pyinstrument.Profiler()
 
-    times = track["iso_times"][:N]
-    coords = [(lat, long) for [long, lat] in track['coordinates'][:N]]
-
-    time_bag = db.from_sequence(times[:10], npartitions=n_workers)
-    #coord_bag = db.from_sequence(coords, npartitions=n_workers)
-
-    # In the paper they use 700, 500, 225
-    def f(times):
-        return lib.track_to_xarray(#,650,750,850
-            times, coords, levels=[225,500], degree_window=40,
-        )
+    times = track["iso_times"]
+    coords = [(lat, long) for [long, lat] in track['coordinates']]
+    levels = [225,500,650,750,850]   # In the tropical cyclone forecasting fusion networks paper they use 700, 500, 225
 
     s_time = time.perf_counter()
     #p.start()
-    chunks = db.map_partitions(f, time_bag)
-    cyclone = xarray.concat(chunks, "time").to_netcdf(f'{get_user_path()}/cyclone_binaries/{ssid}.netcdf',format='NETCDF4',engine='netcdf4')
-    #p.stop()
 
-#    print(f"Saving ndarray for cyclone {ssid}")
-#    with open(f'{get_user_path()}/cyclone_binaries/{ssid}', 'wb') as f:
-#        np.save(f, cyclone)
+    cyclone = lib.track_to_xarray_dataset(times, coords, levels, degree_window=40, n_workers=n_workers)
+    cyclone.to_netcdf(f'{get_user_path()}/cyclone_binaries/{ssid}.nc', format='NETCDF4', engine='netcdf4')
+
+    #p.stop()
     e_time = time.perf_counter()
 
-    print(f"Finished processing cyclone {ssid} in {e_time - s_time:.2f} seconds")
+    print(f"Finished processing cyclone {ssid} in {e_time-s_time:.2f} seconds")
     #p.print(unicode=True, color=True)
 
-def get_some_cyclones_list(list_of_cyclones) -> None:
-    cyclone_thunks = []
-    list_of_tracks = list(tracks.keys())
-
-    for n in list_of_cyclones:
-        job(list_of_tracks[n])
-
 if __name__ == '__main__':
-    print(get_user_path())
+    print("Initialising...")
     client = Client(threads_per_worker=1, local_directory=os.getenv("PBS_JOBFS"))
-    o_time = time.perf_counter()
-    cyclones = list(range(0,1))
-    get_some_cyclones_list(cyclones)
-    n_time = time.perf_counter()
-    print(f"Finished processing {len(cyclones)} cyclones in {n_time-o_time:.2f} seconds")
+    s_time = time.perf_counter()
+
+    track_ids = list(tracks.keys())
+    for ssid in track_ids:
+        create_netcdf_file_for_track(ssid)
+
+    e_time = time.perf_counter()
+    print(f"Finished processing {len(track_ids)} cyclones in {e_time-s_time:.2f} seconds")
---
+++ reanalysis/track_lib.py
@@ -0,0 +1,101 @@
+from typing import List, Tuple, Dict
+import numpy as np
+from datetime import datetime, timezone
+import xarray
+import dask
+import traceback
+from glob import glob
+import dask.bag as db
+
+def netcdf_files(start: datetime, end: datetime, shorthand: str) -> List[Tuple[datetime, str]]:
+    """
+    Gets a list of absolute file paths to all the files needed for reading specified sets of
+    reanalysis data
+    """
+    # Example path: /g/data/rt52/era5/pressure-levels/reanalysis/u/1979/u_era5_oper_pl_19790101-19790131.nc
+    basepath = "/g/data/rt52/era5/pressure-levels/reanalysis"
+    total_months = 12 * (end.year - start.year) + (end.month - start.month) + 1
+    # note that months are 1-indexed
+    year_month_pairs = [(start.year + (start.month + i - 1) // 12, (start.month + i - 1) % 12 + 1) for i in range(total_months)]
+    files = []
+    for (year, month) in year_month_pairs:
+        filename = glob(f"{basepath}/{shorthand}/{year}/{shorthand}_era5_oper_pl_{year}{month:02d}01-*.nc")[0]
+        files.append((
+            datetime(year, month, 1, tzinfo=timezone.utc),
+            filename
+        ))
+    return files
+
+def coord_slice(lat: float, long: float, degrees: float) -> Dict[str, slice]:
+    return {
+        # ERA5 longitude goes from high to low so we slice in opposite directions
+        "latitude": slice(lat + degrees/2, lat - degrees/2),
+        "longitude": slice(long - degrees/2, long + degrees/2)
+    }
+
+def sample_window(ds: xarray.DataArray, degrees: float, lat: float, long: float) -> xarray.DataArray:
+    """
+    Sample ds with the specified window around a lat/long point. Handles wrapping around the
+    periodic X boundary.
+    """
+    points = int(round(degrees / 0.25)) # ERA5 uses a 0.25 deg interval everywhere
+    chunks = []
+    for offset in [-360, 0, 360]:
+        chunks.append(
+            ds.sel(coord_slice(lat, long + offset, degrees))
+              .isel(longitude=slice(None, points), latitude=slice(None, points))
+              .reset_index(["longitude", "latitude"], drop=True)
+        )
+    full = xarray.concat(chunks, "longitude")
+
+    return full.isel(longitude=slice(None, points), latitude=slice(None, points))
+
+def track_single_set_to_xarray(times: List[datetime],
+                               coordinates: List[Tuple[float, float]],
+                               dataset: str,
+                               levels: List[int],
+                               degree_window: int) -> xarray.Dataset:
+    """
+    Sample a track given as times and coordinate locations, at the specified levels, with a given window
+    around each point.
+    """
+    files = netcdf_files(min(times), max(times), dataset)
+
+    ds_array = [
+        xarray.open_mfdataset(
+            filename,
+            chunks={"time": 1, "latitude": 324, "longitude": 360},
+            combine="nested", concat_dim="time"
+        ).sel(level=levels)
+        for start_time, filename in files
+    ]
+
+    res = []
+    for time, (lat, long) in zip(iso_times, coordinates):
+        # get the last dataset with a start time before `time`
+        ds = None
+        start_time = None
+        filename = None
+        for _ds, (_start_time, _filename) in zip(ds_array[::-1], files[::-1]): # loop backwards
+            if np.datetime64(_start_time) <= np.datetime64(time):
+                ds = _ds
+                start_time = _start_time
+                filename = _filename
+        # try to get the window around the coordinate
+        try:
+            res.append(sample_window(ds.sel(time=time), degree_window, lat, long))
+        except Exception as e:
+            traceback.print_exc()
+            print(start_time, filename, time, lat, long, degree_window)
+
+    return xarray.concat(res, "time")
+
+def track_to_xarray_dataset(times: List[str], coordinates: List[Tuple[float, float]],
+                            levels: List[int], degree_window: int, n_workers: int) -> xarray.Dataset:
+    times: list[datetime] = [np.datetime64(iso_time).item() for iso_time in times]
+    sets = ["u", "v", "z", "pv"]
+    sets_bag = db.from_sequence(times, npartitions=n_workers)
+    def f(dataset):
+        return track_single_set_to_xarray(times, coordinates, dataset, levels, degree_window)
+    set_dataarrays = db.map_partitions(f, sets_bag)
+    return xarray.merge(set_dataarrays)
